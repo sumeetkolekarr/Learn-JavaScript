5537bd7c2553799a7cb82b457cc375c785def674
Sumeet Kolekarr <sumeetkolekarr555@gmail.com>
2025-07-02 11:15:30 +0530

    Operators Completed

---
+++ SCS/Operators/index.js
@@ -0,0 +1,42 @@
+// Operators
+
+// Arithmetic
+// +, -, *, /, %, **
+
+// Comparison
+// =, ==, ===, !=, !==, <, >, <=, >=
+
+// Assignment
+// =, +=, -=, *=, /=, %=, **=
+
+// Logical
+// &&, ||, !
+
+// Unary
+// +, -, !, ++, --, typeof
+
+// Bitwise
+// &, |, ^, ~, <<, >>, >>>
+
+// Ternary
+// ? :
+
+// Instanceof is used to check for reference types and mostly typeof is used to check for primitive types
+
+// console.log(a instanceof Number);
+// console.log(typeof NaN);
+// console.log(typeof null);
+// console.log(typeof []);
+// console.log(+'5');
+// console.log(-5);
+// console.log(typeof 5);
+// console.log(!!5);
+// console.log(12==13)
+// console.log(12%5)
+
+// let a = 5
+// console.log(a++ + ++a);
+
+let x = 3
+let y = x++
+console.log(x, y);
