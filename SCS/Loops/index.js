cb1ff29168f2cd76e8a7672459181b0820a807a8
Sumeet Kolekarr <sumeetkolekarr555@gmail.com>
2025-07-04 14:10:20 +0530

    Loops Initiated

---
+++ SCS/Loops/index.js
@@ -0,0 +1,50 @@
+// Looping is repeating a sequence or order
+// For Loop
+// for (let i = 1; i < 101; i++) {
+//   console.log(i);
+// }
+
+// While Loop
+// let i = 1;
+// while (i < 33) {
+//   console.log(i);
+//   i++;
+// }
+
+// Do-While Loop
+// i = 12
+// do {
+//     console.log(i);
+//     i++
+// } while (i<2)
+
+// continue and break keyword
+// for (let i = 1; i < 101; i++) {
+//   if (i == 12) break;
+//   if (i == 10) continue;
+//   console.log(i);
+// }
+
+// Q1. Print Numbers from 1 to 10 using a for loop
+// for (let index = 0; index < 11; index++) {
+//   console.log(index);
+// }
+
+// Q2. Print Numbers from 10 to 1 using a while loop
+// let i = 10
+// while(i>0){
+//     console.log(i);
+//     i--
+// }
+
+// Q3. Print even numbers from 1 to 20 using a for loop
+// for(i=0;i<21;i++){
+//     if (!(i%2)) console.log(i);
+// }
+
+// Q4. Print odd numbers from 1 to 15 using a while loop
+// let i = 1
+// while(i<16){
+//     if (i%2) console.log(i);
+//     i++
+// }
