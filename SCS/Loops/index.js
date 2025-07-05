4d699d92a92c74861a3aaea5359a9e27330f3c12
Sumeet Kolekarr <sumeetkolekarr555@gmail.com>
2025-07-05 14:01:22 +0530

    Loops Completed

--- SCS/Loops/index.js
+++ SCS/Loops/index.js
@@ -48,3 +48,72 @@
 //     if (i%2) console.log(i);
 //     i++
 // }
+
+// Q5 Print the multiplication table of 5
+// for(let i = 1;i<11;i++){
+//     console.log(`5 * ${i} = ${5*i}`);
+// }
+
+// Q6 Find the sum of numbers from 1 to 100
+// let i = 0;
+// sum = 0;
+// while (i < 101) {
+//   sum += i;
+//   i++;
+// }
+// console.log(sum)
+
+// Q7 Print all the numbers between 1 to 50 which are divisible by 3
+// let i = 0;
+// while (i < 101) {
+//   if (i % 3 == 0) {
+//     console.log(i);
+//   }
+//   i++;
+// }
+
+// Q8 Take a number and print each number from 1 to that number if the number is odd or even
+// let num = 6;
+// for (i = 0; i <= num; i++) {
+//   console.log(i % 2 === 0 ? `${i} is even` : `${i} is odd`);
+// }
+
+// Q9 Count how many numbers between 1 to 100 are divisible by both 3 and 5
+// let i = 0;
+// while (i < 101) {
+//   if (i % 3 == 0 && i % 5 == 0) {
+//     console.log(i);
+//   }
+//   i++;
+// }
+
+// Q10 Write a loop from 1 to 100 that prints each number and stops completely when it finds the first number divisible by 7
+// let i = 1;
+// while (i < 101) {
+//   console.log(i);
+//   if (i % 7 === 0) {
+//     break;
+//   }
+//   i++;
+// }
+
+// Q11 Print all the numbers from 1 to 20 and skip the numbers divisible by 3
+// let i = 1;
+// while (i < 20) {
+//   i++;
+//   if (i % 3 === 0) continue;
+//   console.log(i);
+// }
+
+// Q12 Write a loop from 1 to 100 that prints only 5 odd numbers and then stops the loop
+// let i = 0;
+// let counter = 5;
+// while (i < 101) {
+//   i++;
+//   if (i % 2 === 1) {
+//     console.log(i);
+//     counter--;
+//   }
+//   if (!counter) break;
+// }
+
