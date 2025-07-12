fc2ad48353b4d7c6b2edf944779580135f240365
Sumeet Kolekarr <sumeetkolekarr555@gmail.com>
2025-07-12 19:39:31 +0530

    DOM Basics Completed

--- SCS/DOM/script.js
+++ SCS/DOM/script.js
@@ -11,17 +11,29 @@ let abcd = document.querySelector("h1"); // Selects the first element provided a
 // console.dir(abcd);
 
 // Attribute Manipulation
-let a = document.querySelector("a");
-let a2 = document.querySelector(".a2");
-let a3 = document.querySelector(".a3");
-let img = document.querySelector("img");
+// let a = document.querySelector("a");
+// let a2 = document.querySelector(".a2");
+// let a3 = document.querySelector(".a3");
+// let img = document.querySelector("img");
 // console.log(a);
 // a.href = "https://google.com";
 // setAttribute and getAttribute
-a.setAttribute("href", "https://google.com");
-img.setAttribute(
-  "src",
-  "https://images.pexels.com/photos/22800064/pexels-photo-22800064.jpeg?_gl=1*usn6cr*_ga*MTIwMTM2NTczNi4xNzIyNTI0MTM4*_ga_8JE65Q40S6*czE3NTIyMjcyNTEkbzc1JGcxJHQxNzUyMjI3MjY5JGo0MiRsMCRoMA.."
-);
-console.log(a2.getAttribute("href"));
-a3.removeAttribute("href");
+// a.setAttribute("href", "https://google.com");
+// img.setAttribute(
+//   "src",
+//   "https://images.pexels.com/photos/22800064/pexels-photo-22800064.jpeg?_gl=1*usn6cr*_ga*MTIwMTM2NTczNi4xNzIyNTI0MTM4*_ga_8JE65Q40S6*czE3NTIyMjcyNTEkbzc1JGcxJHQxNzUyMjI3MjY5JGo0MiRsMCRoMA.."
+// );
+// console.log(a2.getAttribute("href"));
+// a3.removeAttribute("href");
+
+// CreateElement in DOM
+// let h1 = document.createElement("h1");
+// h1.textContent = "Hello";
+// document.body.prepend(h1); // Inserts at the first
+// document.body.append(h1); // Inserts at the last
+
+// Style Manipulation in JS
+// h1.style.color = 'blue'
+// abcd.classList.add('huli')
+// abcd.classList.remove('huli')
+// abcd.classList.toggle('huli')
