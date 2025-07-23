577d85f9cc214a895d3bb893472fa434d99681df
Sumeet Kolekarr <sumeetkolekarr555@gmail.com>
2025-07-23 16:59:32 +0530

    Ex 3 Completed

--- SCS/Events and Events Handling/Some Advanced Concepts/script.js
+++ SCS/Events and Events Handling/Some Advanced Concepts/script.js
@@ -27,7 +27,37 @@
 // Ex 3
 let form = document.querySelector('form')
 let inputs = document.querySelectorAll('input')
+let main = document.querySelector('#main')
 form.addEventListener('submit', function(dets){
   dets.preventDefault();
-  console.log(inputs[0].value,inputs[1].value,inputs[2].value,inputs[3].value,);
+  // console.log(inputs[0].value,inputs[1].value,inputs[2].value,inputs[3].value);
+  let card = document.createElement('div')
+  card.classList.add('card')
+
+  let profile = document.createElement('div')
+  profile.classList.add('profile')
+
+  let img = document.createElement('img')
+  img.setAttribute('src', inputs[3].value)
+
+  let h3 = document.createElement('h3')
+  h3.textContent = inputs[0].value
+  let h5 = document.createElement('h5')
+  h5.textContent = inputs[1].value
+  let p = document.createElement('p')
+  p.textContent = inputs[2].value
+
+  profile.appendChild(img)
+  card.appendChild(profile)
+  card.appendChild(h3)
+  card.appendChild(h5)
+  card.appendChild(p)
+
+  main.appendChild(card)
+
+  inputs.forEach(function (inp){
+    if(inp.type !== 'submit'){
+      inp.value = ""
+    }
+  })
 })
